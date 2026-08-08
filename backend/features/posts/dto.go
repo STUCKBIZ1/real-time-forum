4bbf9bff9bd69f4058e2db218520aa9bb3a94c82
aelhassn <hassnaouiabdelali89@gmail.com>
2026-08-08 16:28:44 +0100

    handle the query and getpost in repository

--- backend/features/posts/dto.go
+++ backend/features/posts/dto.go
@@ -1,1 +1,28 @@
 package posts
+
+type CreatePostRequest struct {
+	Title   string `json:"title"`
+	Content string `json:"content"`
+}	
+type PostResponse struct {
+	ID        int    `json:"id"`
+	Full_name string `json:"full_name"`
+	Title     string `json:"title"`
+	Content   string `json:"content"`
+	CreatedAt string `json:"created_at"`
+	Likes     int    `json:"likes"`
+	Dislikes  int    `json:"dislikes"`
+	CommentsCount int `json:"commentscount"`
+	Comments []CommentRespose `json:"comments"`
+}
+type GetPostsReq struct {
+	Limit  string `json:"limit"`
+	Cursor string `json:"cursor"`
+}
+type CommentRespose struct{
+	ID int `json:"id"`
+	User_id int `json:"user_id"`
+	Post_id  int `json:"post_id"`
+	Full_name string `json:"full_name"`
+	Content string `json:"content"`
+}
--- backend/features/posts/repository.go
+++ backend/features/posts/repository.go
@@ -11,6 +11,80 @@ func NewRepository(db *sql.DB) *Repository {
 		db: db,
 	}
 }
-func (r *Repository)GetPosts(limit int, cursor int){
-	
+func (r *Repository) GetPosts(limit, offset int) ([]PostResponse, error) {
+	query := `
+		SELECT
+			p.id,
+			u.firstname || ' ' || u.lastname AS full_name,
+			p.title,
+			p.created_at,
+
+			COALESCE(
+				SUM(CASE WHEN pr.reaction = 'like' THEN 1 ELSE 0 END),
+				0
+			) AS likes,
+
+			COALESCE(
+				SUM(CASE WHEN pr.reaction = 'dislike' THEN 1 ELSE 0 END),
+				0
+			) AS dislikes,
+
+			(
+				SELECT COUNT(*)
+				FROM comments c
+				WHERE c.post_id = p.id
+			) AS comments_count
+
+		FROM posts p
+
+		JOIN users u
+			ON u.id = p.user_id
+
+		LEFT JOIN post_reactions pr
+			ON pr.post_id = p.id
+
+		GROUP BY
+			p.id,
+			u.firstname,
+			u.lastname,
+			p.title,
+			p.content,
+			p.created_at
+
+		ORDER BY p.id DESC
+		LIMIT ? OFFSET ?
+	`
+
+	rows, err := r.db.Query(query, limit, offset)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	posts := make([]PostResponse, 0, limit)
+
+	for rows.Next() {
+		var post PostResponse
+
+		err := rows.Scan(
+			&post.ID,
+			&post.Full_name,
+			&post.Title,
+			&post.CreatedAt,
+			&post.Likes,
+			&post.Dislikes,
+			&post.CommentsCount,
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		posts = append(posts, post)
+	}
+
+	if  err != nil {
+		return nil, err
+	}
+
+	return posts, nil
 }
